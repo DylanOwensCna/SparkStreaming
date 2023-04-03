a3bc86173cc1da6be5e3d6b4fe9504ec70739272
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-04-03 15:14:12 -0230

    + Changes to try & filter by user PeacefullyPizza + Added ConfigurationBuilder

--- src/main/java/org/example/TwitterStreamingApp.java
+++ src/main/java/org/example/TwitterStreamingApp.java
@@ -6,6 +6,10 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.twitter.TwitterUtils;
+import twitter4j.Status;
+import twitter4j.auth.Authorization;
+import twitter4j.auth.AuthorizationFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.util.Arrays;
 
@@ -19,6 +23,13 @@ public class TwitterStreamingApp {
         String accessTokenSecret = System.getenv("TWITTER_ACCESS_SECRET");
         System.setProperty("twitter4j.config", "twitter4j.properties");
 
+        // Create a Twitter configuration
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret);
+        Authorization authorization = AuthorizationFactory.getInstance(configurationBuilder.build());
 
         // Create a Spark configuration
         SparkConf conf = new SparkConf().setAppName("TwitterStream");
@@ -26,8 +37,9 @@ public class TwitterStreamingApp {
         // Create a Streaming context with a batch interval of 5 seconds
         JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
-        // Create a Twitter DStream
-        JavaDStream<twitter4j.Status> stream = TwitterUtils.createStream(ssc);
+        // Create a Twitter DStream of your own tweets
+        JavaDStream<Status> stream = TwitterUtils.createStream(ssc, authorization)
+                .filter(status -> status.getUser().getScreenName().equals("PeacefullyPizza"));
 
         // Extract hashtags from tweets
         JavaDStream<String> hashtags = stream.flatMap(status -> Arrays.asList(status.getText().split(" ")).iterator())
